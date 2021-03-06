6b13f4ba97993b5160d3c8c6efa2c45f7d60a623
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2021-03-05 22:49:28 -0300

    fix: :bug: Fix setup
    Fix setup to upgrade packages

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import sys
 import subprocess
 
 if sys.platform.startswith('win'):
-  subprocess.run(["pip", "install", "-r", "requirements.txt"])
+  subprocess.run(["pip", "install","--upgrade","--force-reinstall", "-r", "requirements.txt"])
   subprocess.run(["python","main.py"])
 else:
   try:
-    subprocess.run(["python3","-m","pip", "install", "-r", "requirements.txt"])
-    subprocess.run(["pythone","main.py"])
+    subprocess.run(["python3","-m","pip", "install","--upgrade","--force-reinstall", "-r", "requirements.txt"])
+    subprocess.run(["python","main.py"])
   except:
     print("[ERROR]: Erro ao instalar os requirements. Instale os requirements manualmente pelo requirements.txt. Use algo parecido com 'pip install -r requirements.txt'")
